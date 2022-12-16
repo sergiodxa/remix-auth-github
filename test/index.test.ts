3ecc09ae5571bd43dcfb1ebb367ead843a399fbd
Tom Rowe <theflyingcoder@hotmail.com>
2022-12-16 11:24:29 +1000

    Adding the tests for typed array

--- test/index.test.ts
+++ test/index.test.ts
@@ -46,6 +46,33 @@ describe(GitHubStrategy, () => {
     }
   });
 
+  test("should allow typed scope array", async () => {
+    let strategy = new GitHubStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+        scope: ["read:user"],
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/github");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, BASE_OPTIONS);
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.searchParams.get("scope")).toBe("read:user");
+    }
+  });
+
   test("should have the scope `user:email` as default", async () => {
     let strategy = new GitHubStrategy(
       {
